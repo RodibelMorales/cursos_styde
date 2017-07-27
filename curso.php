0aff08721cad0e124c3060f90541efdfd5707611
Rodibel Morales <rmorales@topmre.com>
2017-07-27 17:09:13 -0500

    tema encapsulamiento

--- curso.php
+++ curso.php
@@ -5,18 +5,34 @@
 	class Persona{
 		public $nombre;
 		public $apellidoPaterno;
+		protected $direccion;
 		
-		public function __construct($nombre,$apellido){
+		public function __construct($nombre,$apellido,$direccion){
 			$this->nombre=$nombre;
 			$this->apellidoPaterno=$apellido;
+			$this->direccion=$direccion;
 		}
-		private function nombreCompleto(){
+		//Metodo Geter
+		public function getnombreCompleto(){
 			return $this->nombre. ' ---- '.$this->apellidoPaterno;
+		}
+		//Metodo Geter
+		public function getDireccion(){
+			return $this->direccion;
+		}
+		public function setDireccion($nuevaDireccion){
+			$this->direccion=$nuevaDireccion;
+		}
+		public function setNombre($nombreNuevo){
+			$this->nombre=$nombreNuevo;
 		}
 	}
 
-$NuevaPersona= new Persona('Cris','Tellez');
-echo $NuevaPersona->nombreCompleto();
+$NuevaPersona= new Persona('Rodibel','Morales','Calle 30 por el centro de la avenida XD');
 
 
+$NuevaPersona->setNombre('Rodi');
+$NuevaPersona->setDireccion('ejemplo');
+echo $NuevaPersona->getDireccion()."<br>";
+echo $NuevaPersona->getnombreCompleto();
 ?>
